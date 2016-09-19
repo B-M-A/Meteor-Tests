cdd7c3e295976d2ff03bddd891ada443995fcd1b
Xingchen Hong <hello@xc-h.net>
2016-09-19 15:37:55 -0500

    Reproduced the issue.
    Do something like:
    
    ```JavaScript
    infDb.setServerParameters(JSON.parse('{"search":"b"}')).then(function () {console.info('done', infDb.find({}).fetch())});
    ```
    
    And you'll see that the request never gets resolved.

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,7 +1,11 @@
+import { Mongo } from 'meteor/mongo';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { InfiniLoad } from "meteor/zodiase:infinite-load";
 
 import './main.html';
+
+const db = new Mongo.Collection('data');
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -19,4 +23,13 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
+});
+
+Meteor.startup(() => {
+  const infDb = window.infDb = new InfiniLoad(db, {
+    verbose: true
+  });
+  infDb.start().then((infLd) => {
+    console.log('infDb.stats', infLd.stats);
+  });
 });
--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -1,5 +1,45 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { InfiniLoad } from "meteor/zodiase:infinite-load";
+
+const db = new Mongo.Collection('data');
+// Set indexes
+db._ensureIndex({ createTime: 1 });
+db._ensureIndex({ name: "text" });
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  const expectedDataCount = 40;
+  const currentDataCount = db.find({}).count();
+  console.log('currentDataCount', currentDataCount);
+  // Create initial data.
+  if (currentDataCount !== expectedDataCount) {
+    console.log('Initializing data...');
+    db.remove({});
+    let time = Date.now();
+    for (let i = 0; i < expectedDataCount; ++i) {
+      db.insert({
+        name: ['a', 'b', 'c'][i % 3],
+        createTime: time + i
+      });
+    }
+    console.log(`${expectedDataCount} data items inserted`);
+  }
+
+  const infDb = new InfiniLoad(db, {
+    'selector' (userId, args) {
+      const search = args.search || '';
+
+      return search
+      ? {
+          '$text': {
+            '$search': search
+          }
+        }
+      : {};
+    },
+    verbose: true
+  });
+
 });
