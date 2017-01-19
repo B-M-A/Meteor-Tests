96552e39ac527c90a6936db6b20bca5d2e9ef481
Xingchen Hong <hello@xc-h.net>
2017-01-19 17:22:02 -0600

    Add test code.

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,11 +1,23 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import '@material/button/dist/mdc.button.css';
+import { MDCRipple } from '@material/ripple/dist/mdc.ripple.js';
+import '@material/ripple/dist/mdc.ripple.css';
+import '@material/typography/dist/mdc.typography.css';
+
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+});
+
+Template.hello.onRendered(function helloOnRendered() {
+  // Initialize all MDC-Ripples.
+  this.$('.mdc-ripple-surface').each((index, element) => {
+    MDCRipple.attachTo(element);
+  });
 });
 
 Template.hello.helpers({
