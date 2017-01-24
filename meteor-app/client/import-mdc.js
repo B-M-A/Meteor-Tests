f0e0b8438d1b24408dabecf3dc87defcebe95df2
Xingchen Hong <hello@xc-h.net>
2017-01-24 16:10:10 -0600

    Remove import from npm.

--- meteor-app/client/import-mdc.js
+++
@@ -1,9 +0,0 @@
-// Test importing MDC from the distribution files.
-
-import mdc from 'material-components-web/dist/material-components-web.js';
-import mdc_min from 'material-components-web/dist/material-components-web.min.js';
-
-import 'material-components-web/dist/material-components-web.css';
-
-window.mdc = mdc;
-window.mdc_min = mdc_min;
