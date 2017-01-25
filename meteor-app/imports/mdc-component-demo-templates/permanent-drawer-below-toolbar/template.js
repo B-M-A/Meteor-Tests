16481088fcbab3c202b1b3b216b7accba8f23f52
Xingchen Hong <hello@xc-h.net>
2017-01-25 11:43:00 -0600

    Add two other drawer demos.

---
+++ meteor-app/imports/mdc-component-demo-templates/permanent-drawer-below-toolbar/template.js
@@ -0,0 +1,23 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+// Import MDC components from the meteor package.
+import { } from 'meteor/zodiase:mdc-styleless';
+
+import './template.html';
+
+const tplName = "mdc_demo_permanent_drawer_below_toolbar",
+      tplClass = Template[tplName];
+
+tplClass.onCreated(function templateOnCreated() {
+});
+
+tplClass.onRendered(function templateOnRendered() {
+  import './template.css';
+});
+
+tplClass.helpers({
+});
+
+tplClass.events({
+});
--- meteor-app/imports/startup/client/main.js
+++ meteor-app/imports/startup/client/main.js
@@ -11,6 +11,8 @@ import '/imports/mdc-component-demo-templates/button/template';
 import '/imports/mdc-component-demo-templates/card/template';
 import '/imports/mdc-component-demo-templates/checkbox/template';
 import '/imports/mdc-component-demo-templates/temporary-drawer/template';
+import '/imports/mdc-component-demo-templates/permanent-drawer-above-toolbar/template';
+import '/imports/mdc-component-demo-templates/permanent-drawer-below-toolbar/template';
 
 import './main.html';
 import './main.css';
